bce4132516a72fbdf56803d82303bed2760b61cf
Rakshit-Varshney <75945445+Rakshit-Varshney@users.noreply.github.com>
2021-10-16 13:34:54 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -9,14 +9,22 @@ recognition.onresult=function(event){
     var content=event.results[0][0].transcript;
     console.log(content);
     document.getElementById("textbox").innerHTML=content;
-    speak();
+    if (content=='take my selfie'){
+        console.log("taking selfie");
+        speak();
+    }
+   
 }
 function speak(){
     var synth=window.speechSynthesis;
-    speak_data=document.getElementById("textbox").value;
+    speak_data="taking your selfie in 5 seconds";
     var utter_this=new SpeechSynthesisUtterance(speak_data);
     synth.speak(utter_this);
 Webcam.attach(camera);
+setTimeout(function(){
+    take_snapshot();
+    save();
+},5000);
 }
 Webcam.set({
     width:360,
@@ -25,3 +33,14 @@ Webcam.set({
     png_quality:90
 });
 camera=document.getElementById("camera");
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'">';
+    });
+}
+function save(){
+    link=document.getElementById("link");
+    image=document.getElementById("selfie_image").src;
+link.href=image;
+link.click();
+}
